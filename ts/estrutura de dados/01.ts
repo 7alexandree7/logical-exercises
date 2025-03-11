a4b1c10ae5b50c9300cdeb9b0c8effeab4c2039c
alexandre <alexandre020602@gmail.com>
2025-03-11 20:07:21 -0300

    01 exercicio de estrutura de dados

--- ts/estrutura de dados/01.ts
+++ ts/estrutura de dados/01.ts
@@ -1,1 +1,29 @@
-console.log("hello word")
+interface Pessoa {
+    nome: string;
+    idade: number;
+    sexo: string;
+    idiomas: string[];
+}
+
+const pessoas: Record<string, Pessoa> = {
+
+    pessoa01:  {
+        nome: 'unfast',
+        idade: 20,
+        sexo: 'masculino',
+        idiomas: ['português', 'ingles', 'español'],
+    },
+
+    pessoa02:{
+        nome: 'alice',
+        idade: 18,
+        sexo: 'feminino',
+        idiomas: ["arabe", "russo", "japones"],
+    },
+}
+
+const pessoa01 = pessoas.pessoa01
+console.log(pessoa01)
+
+const pessoa02 = pessoas.pessoa02
+console.log(pessoa02)
