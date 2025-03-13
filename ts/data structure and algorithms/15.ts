f24460c47e872a5f7803d486f059d52a12953a46
alexandre <alexandre020602@gmail.com>
2025-03-12 22:52:57 -0300

    tipando

--- ts/data structure and algorithms/15.ts
+++ ts/data structure and algorithms/15.ts
@@ -1,8 +1,8 @@
 //Dada uma string,
 //substitua todas as ocorrências de uma palavra específica por outra.
 
-const text = "O carro vermelho está estacionado ao lado do carro azul.";
+const text: string = "O carro vermelho está estacionado ao lado do carro azul.";
 // TODO: Substituir "carro" por "veículo"
 
-const newText = text.split(" ").map((word) => word === "carro" ? word.replace("carro", "veículo") : word).join(" ")
+const newText: string = text.split(" ").map((word) => word === "carro" ? word.replace("carro", "veículo") : word).join(" ")
 console.log(newText)
