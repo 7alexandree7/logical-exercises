8c887f12edd5555f82cd125e8f2aea9432ca0ded
alexandre <alexandre020602@gmail.com>
2025-04-08 10:59:25 -0300

    treinando o reducer, conversão de array em obj (key personalizadas

---
+++ ts/data structure and algorithms/list of chatgpt exercises/32.ts
@@ -0,0 +1,23 @@
+//Transformar array em objeto indexado
+//Transforme um array de { id, name } em um objeto no formato { [id]: name }.
+
+type Iuser45 = {
+    id: number,
+    name: string
+}
+
+const user45: Iuser45[] = [
+    {id: 10, name: 'John'},
+    {id: 20, name: 'Jane'},
+    {id: 30, name: 'Doe'},
+    {id: 40, name: 'Smith'},
+    {id: 50, name: 'Emily'},
+]
+
+const indexUsers45 = user45.reduce((acumulador: any, itemAtual: any) => {
+    acumulador[itemAtual.id] = itemAtual
+    return acumulador
+}, {} as Record<number, Iuser45>)
+
+
+console.log(indexUsers45) 
---
+++ ts/data structure and algorithms/list of chatgpt exercises/33.ts
@@ -0,0 +1,23 @@
+//Transformar array em objeto indexado
+//Transforme um array de { id, name } em um objeto no formato { [id]: name }.
+
+type Users22 = {
+    id: number,
+    name: string
+}
+
+const users22: Users22[] = [
+    {id: 100, name: 'alexandre'},
+    {id: 200, name: 'andre'},
+    {id: 300, name: 'andreia'},
+    {id: 400, name: 'andreza'},
+]
+
+const indexUsers22 = users22.reduce((acumulador: any, itemAtual: any) => {
+
+    acumulador[itemAtual.id] = itemAtual
+    return acumulador
+
+}, {} as Record<number, Users22>)
+
+console.log(indexUsers22)
