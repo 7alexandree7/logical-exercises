56bffd6fab672f825b6a3b26d6cf2643c7660daa
alexandre <alexandre020602@gmail.com>
2025-04-08 19:02:46 -0300

    finalizando o exercicio 34, relembrando o metodo trim()

---
+++ ts/data structure and algorithms/list of chatgpt exercises/34.ts
@@ -0,0 +1,7 @@
+//Limpar espaços de uma string digitada
+//Remova espaços extras no início e fim de uma string.
+const removerEspacos = (string: string) => {
+    return string.trim()
+}
+
+console.log(removerEspacos("   Olá, Mundo!   "))
